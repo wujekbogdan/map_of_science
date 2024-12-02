72e43adda77d6022ab421c6d838b7b4ad591ac85
wujekbogdan <533954+wujekbogdan@users.noreply.github.com>
2024-12-02 09:38:31 +0100

    chore: cleanup

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-comlink/client" />
-/// <reference types="vite-plugin-svgr/client" />
-// TODO: Remove svgr before merge
 
 type StyleObject = {
   [name: string]: string;

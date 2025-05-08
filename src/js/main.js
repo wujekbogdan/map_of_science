25a8a7b4bf5a3c67cb95ebd4ca25243771074c95
wujekbogdan <533954+wujekbogdan@users.noreply.github.com>
2025-05-08 18:28:09 +0200

    chore: remove legacy css/js

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,7 @@
 import * as points from "./points.js";
 
-import "../css/annotation.css";
 import "../css/article.css";
-import "../css/common.css";
 import "../css/content.css";
-import "../css/labels.css";
-
-import "animate.css";
 
 export const init = () => {
   points.load();

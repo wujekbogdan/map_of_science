b60bc77c458fbc02fefd930b7f97482fff4db6b7
wujekbogdan <wujekbogdan@users.noreply.github.com>
2025-05-09 00:21:21 +0200

    CSV parsing with Web Workers (#38)
    * ci(workflow): deployment of dev branch
    
    * ci(workflows): fix dev deployment
    
    * chore: wip
    
    * chore: define tsv schemas
    
    * chore: wip
    
    * chore: parseFromUrlWithSchema refactor
    
    * chore: schema tests fix
    
    * chore: simplify mapCollector
    
    * chore: mapCollector - add indexBy named param
    
    * chore: wip
    
    * chore: refactor points.js to use ponts.ts
    
    * ci(dev-release): set VITE_BASE_URL
    
    * chore: add changelog | fix tooltips
    
    * chore: bring back csv parsing with Web Workers
    
    * chore: changelog update
    
    * fix: shared worker to regular worker
    
    * feat: strongly typed csv parsing (#52)
    
    Co-authored-by: wujekbogdan <533954+wujekbogdan@users.noreply.github.com>
    
    * fix(zoom): the cities svg resized correctly when window resized (#53)
    
    ---------
    
    Co-authored-by: Szymon Bednorz <dsonyyy@gmail.com>
    Co-authored-by: wujekbogdan <533954+wujekbogdan@users.noreply.github.com>

---
+++ .changeset/spotty-beers-fold.md
@@ -0,0 +1,5 @@
+---
+"map_of_science": patch
+---
+
+Fixed the cities svg behaviour to resize when the window is resized.
---
+++ src/api/index.ts
@@ -0,0 +1,66 @@
+import { createProcessor, withHttpProvider } from "../csv/parse.ts";
+import { DataSchema, ConceptSchema, CityLabelSchema } from "../schema";
+import { z, ZodSchema, ZodTypeDef } from "zod";
+import { arrayCollector, mapCollector } from "../csv/collector.ts";
+
+type Options<T> = {
+  url: string;
+  schema: ZodSchema<T, ZodTypeDef, unknown>;
+};
+
+const loadAsArray = async <T>({ url, schema }: Options<T>) => {
+  const collector = arrayCollector<T>();
+  const processor = createProcessor(schema, collector);
+
+  await withHttpProvider(url, processor.process);
+
+  return processor.getResults();
+};
+
+const loadAsMap = async <T, K>({
+  url,
+  schema,
+  getKey,
+}: Options<T> & { getKey: (item: T) => K }) => {
+  const collector = mapCollector<K, T>(getKey);
+  const processor = createProcessor(schema, collector);
+
+  await withHttpProvider(url, processor.process);
+
+  return processor.getResults();
+};
+
+/**
+ * - City labels loading doesn't depend on anything.
+ * - Concepts loading doesn't depend on anything either.
+ * - Data points depend only on city labels.
+ *
+ * To maximize parallelism, we can load city labels and concepts in parallel
+ * and then wait for city labels to finish before loading data points.
+ */
+export const loadData = async () => {
+  const loadingConcepts = loadAsMap({
+    url: new URL("../../asset/keys.tsv", import.meta.url).href,
+    schema: ConceptSchema(z),
+    getKey: (item) => item.index,
+  });
+
+  const loadingLabels = loadAsMap({
+    url: new URL("../../asset/labels.tsv", import.meta.url).href,
+    schema: CityLabelSchema(z),
+    getKey: (item) => item.clusterId,
+  });
+
+  // We're awaiting the labels promise only because at this point we don't care about concepts yet
+  const labels = await loadingLabels;
+
+  const [concepts, dataPoints] = await Promise.all([
+    loadingConcepts,
+    loadAsArray({
+      url: new URL("../../asset/data.tsv", import.meta.url).href,
+      schema: DataSchema(z, labels),
+    }),
+  ]);
+
+  return { concepts, labels, dataPoints };
+};

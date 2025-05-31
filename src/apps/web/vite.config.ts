f6d511300b14dfe35dd786c1fa1316de712ee9cb
wujekbogdan <533954+wujekbogdan@users.noreply.github.com>
2025-05-31 14:43:17 +0200

    chore: dev mode type checking

--- src/apps/web/vite.config.ts
+++ src/apps/web/vite.config.ts
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, loadEnv } from "vite";
+import checker from "vite-plugin-checker";
 import { comlink } from "vite-plugin-comlink";
 import svgMapParser from "./vite-plugin/svg-map-parser";
 
@@ -7,7 +8,17 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    plugins: [react(), comlink(), svgMapParser()],
+    plugins: [
+      checker({
+        typescript: {
+          root: "./",
+          tsconfigPath: "tsconfig.react.json",
+        },
+      }),
+      react(),
+      comlink(),
+      svgMapParser(),
+    ],
     base: env.VITE_BASE_URL,
     root: "src",
     envDir: "../",
